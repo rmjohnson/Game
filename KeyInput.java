417a742146dd469f343ffd6bd476602c3cd48cb1
Ryan Johnson <kritstov@gmail.com>
2011-04-09 01:32:27 -0700

    Yo dawg I heard you like scrolling words so I added scrolling words with a random x-value. Things to add: text hi-lighting when keys are pressed, make the cords array scale to the words array, and figure out how to properly space the words (maybe just keep them all on seperate rows by scaling the y value by it's place in the word array.) I think that's a pretty good todo list for now.

--- KeyInput.java
+++ KeyInput.java
@@ -6,46 +6,123 @@ import java.lang.*;
 import java.util.Random;
 
 public class KeyInput extends Applet
-   implements KeyListener, MouseListener {
+implements KeyListener, MouseListener, Runnable {
 
-   int width, height;
-   int x, y;
-   String s = "";
+	int width, height;
+	int x, y;
+	String s = "";
+	Thread t = null;
+	boolean threadSuspended;
 
-   public void init() {
-      width = getSize().width;
-      height = getSize().height;
-      setBackground( Color.black );
+	Random generator = new Random();
+	String[] words = {"Zerg","Protoss","Terran"};
+	int[][] cords = {{0,5},{0,25},{0,45}};
 
-      //Random generator = new Random();
-      //int r = generator.nextInt();
-      x = 5;
-      y = 5;
+	public void init() {
+		width = getSize().width;
+		height = getSize().height;
+		setBackground( Color.black );
 
-      addKeyListener( this );
-      addMouseListener( this );
-   }
+		for (int i=0; i < cords.length; i++)
+		{
+			int r = generator.nextInt(100)+100;
+			cords[i][0] = r;
+		}
+		addKeyListener( this );
+		addMouseListener( this );
+	}
 
-   public void keyPressed( KeyEvent e ) { }
-   public void keyReleased( KeyEvent e ) { }
-   public void keyTyped( KeyEvent e ) {
-      char c = e.getKeyChar();
-      if ( c != KeyEvent.CHAR_UNDEFINED ) {
-         s = Character.toString(c);
-         repaint();
-         e.consume();
-      }
-   }
+	public void destroy()
+	{
+		System.out.println("destroy()");
+	}
 
-   public void mouseEntered( MouseEvent e ) { }
-   public void mouseExited( MouseEvent e ) { }
-   public void mousePressed( MouseEvent e ) { }
-   public void mouseReleased( MouseEvent e ) { }
-   public void mouseClicked( MouseEvent e ) {
-   }
+	public void start()
+	{
+		System.out.println("start(): begin");
+		if ( t == null )
+		{
+			System.out.println("start(): creating thread");
+			t = new Thread( this );
+			System.out.println("start(): starting thread");
+			threadSuspended = false;
+			t.start();
+		}
+		else
+		{
+			if ( threadSuspended )
+			{
+				threadSuspended = false;
+				System.out.println("start(): notifying thread");
+				synchronized( this )
+				{
+					notify();
+				}
+			}
+		}
+		System.out.println("start(): end");
+	}
 
-   public void paint( Graphics g ) {
-      g.setColor( Color.red );
-      g.drawString( s, x, y );
-   }
+	public void stop()
+	{
+		System.out.println("stop(): begin");
+		threadSuspended = true;
+	}
+	public void run()
+	{
+		System.out.println("run(): begin");
+		try {
+			while (true) {
+				System.out.println("run(): awake");
+
+				for(int i=0;i < cords.length; i++)
+				{
+					cords[i][1]++;
+				}
+
+				if ( threadSuspended ) 
+				{
+					synchronized( this )
+					{
+						while ( threadSuspended )
+						{
+							System.out.println("run(): waiting");
+							wait();
+						}
+					}
+				}
+				System.out.println("run(): requesting repaint");
+				repaint();
+				System.out.println("run(): sleeping");
+				t.sleep ( 50 );
+			}
+		}
+		catch (InterruptedException e) { }
+		System.out.println("run(): end");
+	}
+	public void keyPressed( KeyEvent e ) { }
+	public void keyReleased( KeyEvent e ) { }
+	public void keyTyped( KeyEvent e ) {
+		char c = e.getKeyChar();
+		if ( c != KeyEvent.CHAR_UNDEFINED ) {
+			s = Character.toString(c);
+			repaint();
+			e.consume();
+		}
+	}
+
+	public void mouseEntered( MouseEvent e ) { }
+	public void mouseExited( MouseEvent e ) { }
+	public void mousePressed( MouseEvent e ) { }
+	public void mouseReleased( MouseEvent e ) { }
+	public void mouseClicked( MouseEvent e ) {
+	}
+
+	public void paint( Graphics g ) {
+		g.setColor( Color.red );
+		for(int i=0; i<cords.length; i++)
+		{
+			g.drawString(words[i],cords[i][0],cords[i][1]);
+		}
+	}
 }
